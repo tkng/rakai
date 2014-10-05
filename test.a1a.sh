0f5536a9b69905dc72a487c8206a7761d953301c
TOKUNAGA Hiroyuki <tkng@preferred.jp>
2014-10-05 17:27:18 +0900

    correspond to adagrad

--- test.a1a.sh
+++ test.a1a.sh
@@ -7,5 +7,9 @@ if [ ! -f a1a.t ]; then
   curl http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a1a.t > a1a.t
 fi
 
-./rakai/rakai train -a perceptron -m a1a.perceptron.model -i 10 a1a > /dev/null && ./rakai/rakai test -m a1a.perceptron.model a1a.t
-./rakai/rakai train -a nbsvm -m a1a.nbsvm.model -i 10 a1a > /dev/null && ./rakai/rakai test -m a1a.nbsvm.model a1a.t
+./rakai/rakai train -a perceptron -m a1a.perceptron.model -i 50 a1a > /dev/null && ./rakai/rakai test -m a1a.perceptron.model a1a.t > a1a.perceptron.result
+./rakai/rakai train -a svm -m a1a.svm.model --adagrad=false -i 50 a1a > /dev/null && ./rakai/rakai test -m a1a.svm.model a1a.t > a1a.svm.result
+./rakai/rakai train -a nbsvm -m a1a.nbsvm.model -i 50 --adagrad=false a1a > /dev/null && ./rakai/rakai test -m a1a.nbsvm.model a1a.t  > a1a.nbsvm.result
+
+./rakai/rakai train -a svm -m a1a.svm.model -i 50 --adagrad=true a1a > /dev/null && ./rakai/rakai test -m a1a.svm.model a1a.t > a1a.svm.ada.result
+./rakai/rakai train -a nbsvm -m a1a.nbsvm.model -i 50 --adagrad=true a1a > /dev/null && ./rakai/rakai test -m a1a.nbsvm.model a1a.t > a1a.nbsvm.ada.result
